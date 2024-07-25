67f1422c2885120b7638380630d1c3fee3d07d70
KEVIN CONDOR <kevin.condor@epn.edu.ec>
2024-07-24 20:27:40 -0500

    feat: Add null check for getCookie function

--- src/helpers/Credentials.ts
+++ src/helpers/Credentials.ts
@@ -1,12 +1,15 @@
 
 function getCookie(name: string): string | null {
+    if (typeof window === 'undefined') {
+        return null;
+    }
+    
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
         return parts.pop()?.split(';').shift() || null;
     }
     return null;
-    // return 'null';
 }
 
 export {
